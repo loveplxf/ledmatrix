04cd07cd5e43d7194f4e6e8681f60cbf253a1518
loveplxf <hubin0809@126.com>
2020-01-09 21:18:14 +0800

    Add files via upload

---
+++ ledmatrix/main.ts
@@ -0,0 +1,39 @@
+
+/**
+ * 使用此文件来定义自定义函数和图形块。
+ * 想了解更详细的信息，请前往 https://makecode.microbit.org/blocks/custom
+ */
+
+enum MyEnum {
+    //% block="one"
+    One,
+    //% block="two"
+    Two
+}
+
+/**
+ * 自定义图形块
+ */
+//% weight=100 color=#0fbc11 icon=""
+namespace ledmatrix {
+    /**
+     * TODO: 在此处描述您的函数
+     * @param n 在此处描述参数, eg: 5
+     * @param s 在此处描述参数, eg: "Hello"
+     * @param e 在此处描述参数
+     */
+    //% block
+    export function foo(n: number, s: string, e: MyEnum): void {
+        // Add code here
+        basic.showNumber(8)
+    }
+
+    /**
+     * TODO: 在此处描述您的函数
+     * @param value 在此处描述”值“, eg: 5
+     */
+    //% block
+    export function fib(value: number): number {
+        return value <= 1 ? value : fib(value -1) + fib(value - 2);
+    }
+}
